c4405e7d49feb8785a27b1a2bb03f54b01c2fa92
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-03-22 12:22:03 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayerObject.hpp>
-#include <Geode/modify/CCParticleSystem.hpp>
 #include <Geode/Utils.hpp>
 
 // this is really ugly
 
 using namespace geode::prelude;
 
-class $modify(MyParticleSystem, CCParticleSystem) {
-	auto getPParticles() {
-		return m_pParticles;
-	}
-
-    void forceColor(const ccColor4F& col) {
-        this->CCParticleSystem::setStartColor(col);
-        this->CCParticleSystem::setEndColor(col);
-
-        // auto pp = this->getPParticles();
-        // size_t count = this->CCParticleSystem::getParticleCount();
-
-        // for (size_t i = 0; i < count; i++) {
-        //     tCCParticle& p = pp[i];
-        //     p.color = col;
-        // }
-    }
-};
+void forceColor(CCParticleSystem* system, const ccColor4F& col) {
+    // virtuals are broken in geode
+    system->CCParticleSystem::setStartColor(col);
+    system->CCParticleSystem::setEndColor(col);
+}
 
 static inline ccColor3B lerpColor(ccColor3B from, ccColor3B to, float delta) {
     delta = std::clamp(delta, 0.f, 1.f);
@@ -58,18 +44,6 @@ class $modify(HookedPlayerObject, PlayerObject) {
 	bool init(int p0, int p1, GJBaseGameLayer* p2, cocos2d::CCLayer* p3, bool p4) {
 		if (!PlayerObject::init(p0, p1, p2, p3, p4)) return false;
 
-        // log::debug("m_unk6dc: {}", m_unk6dc);
-        // log::debug("m_trailingParticles: {}", m_trailingParticles);
-        // log::debug("m_shipClickParticles: {}", m_shipClickParticles);
-        // log::debug("m_unk6e8: {}", m_unk6e8);
-        // log::debug("m_ufoClickParticles: {}", m_ufoClickParticles);
-        // log::debug("m_robotBurstParticles: {}", m_robotBurstParticles);
-        // log::debug("m_unk6f4: {}", m_unk6f4);
-        // log::debug("m_swingBurstParticles1: {}", m_swingBurstParticles1);
-        // log::debug("m_swingBurstParticles2: {}", m_swingBurstParticles2);
-        // log::debug("m_unk704: {}", m_unk704);
-        // log::debug("m_unk708: {}", m_unk708);
-
         this->schedule(schedule_selector(HookedPlayerObject::updateGlowColorD));
 
 		return true;
@@ -90,20 +64,18 @@ class $modify(HookedPlayerObject, PlayerObject) {
             }
         }
 
-        // log::debug("forcing to {}", newColor);
-
         auto col = ccc4FFromccc3B(newColor);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_unk6dc))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_trailingParticles))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_shipClickParticles))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_unk6e8))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_ufoClickParticles))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_robotBurstParticles))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_unk6f4))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_swingBurstParticles1))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_swingBurstParticles2))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_unk704))->forceColor(col);
-		static_cast<MyParticleSystem*>(static_cast<CCParticleSystem*>(m_unk708))->forceColor(col);
+		forceColor(m_unk6dc, col);
+		forceColor(m_trailingParticles, col);
+		forceColor(m_shipClickParticles, col);
+		forceColor(m_unk6e8, col);
+		forceColor(m_ufoClickParticles, col);
+		forceColor(m_robotBurstParticles, col);
+		forceColor(m_unk6f4, col);
+		forceColor(m_swingBurstParticles1, col);
+		forceColor(m_swingBurstParticles2, col);
+		forceColor(m_unk704, col);
+		forceColor(m_unk708, col);
     }
 
     void updateGlowColor() {
